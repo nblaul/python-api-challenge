a9c3e4c2b6537ecd7bb6cd9a5978dbf1dfeca83e
Nicholas Blaul <nicholas.blaul@gmail.com>
2021-08-11 03:52:36 -0500

    updated api keys and .gitignore file

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "cddd837858705b6b4327fed093aae810"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDnzurOx0YqSZ8mzQ1WPWz2l2wyjfC77tE"
